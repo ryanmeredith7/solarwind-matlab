934c653765eb12f85aec9e558ff176f7893baa9c
Ryan Meredith <ryan.meredith@unb.ca>
2020-08-05 11:55:07 -0300

    Viewing plots

--- test.m
+++ test.m
@@ -9,93 +9,27 @@ jumps = datetime(readmatrix("data/jumps.txt", "OutputType", "string", ...
 
 for jump = jumps
 
-    [omniData, omniTime] = getOmni(jump);
-
-    a = dateshift(jump, "start", "hour");
-    b = a + minutes(59);
-    inds = a <= omniTime & omniTime <= b;
-    x = omniData(inds);
-    t = omniTime(inds);
-
-    while all(isnan(x(t < jump)))
-        a = a - hours(1);
-        inds = a <= omniTime & omniTime <= b;
-        x = omniData(inds);
-        t = omniTime(inds);
-    end
-
-    while all(isnan(x(t > jump)))
-        b = b + hours(1);
-        inds = a <= omniTime & omniTime <= b;
-        x = omniData(inds);
-        t = omniTime(inds);
-    end
-
-    myplot("omni", x, jump, t);
+    folder = fullfile("data", "figures", string(jump));
+    omniFig = openfig(fullfile(folder, "omni.fig"));
 
     for loc = locs
+        items = dir(fullfile(folder, loc + "*.fig"));
+        if isempty(items), continue; end
+        n = length(items);
+        figs = gobjects(n, 1);
 
-        tecData = table();
-        next = false;
-
-        for d = a:hours(1):b
-            try
-                tecData = [tecData; getTec(d, loc)];
-            catch err
-                if err.identifier == "ISMR:NoData" || err.message == "File is empty."
-                    next = true;
-                    break;
-                else
-                    rethrow(err);
-                end
-            end
+        for i = 1:n
+            figs(i) = openfig(fullfile(folder, items(i).name));
         end
 
-        if next; continue; end
+        m = msgbox("Next?", "Next");
+        uiwait(m);
 
-        tecData = retime(processTec(tecData), "minutely", "mean");
-        tecData(1,:) = [];
-
-        for var = tecData.Properties.VariableNames
-            var = var{1};
-            myplot(loc + "-" + var, tecData{:,var}, jump);
-        end
+        valid = ishandle(figs);
+        if any(valid), close(figs(valid)); end
 
     end
 
-end
-
-function myplot(name, x, jump, t)
-    persistent fig ln tl xln
-
-    if isempty(fig)
-
-        fig = figure("Name", name, "WindowStyle", "docked", "NumberTitle", false);
-        ln = line(t, x);
-        xln = xline(jump);
-        tl = title(name + " - " + string(jump), "Interpreter", "none");
-
-    else
-
-        if nargin == 4
-            ln.XData = t;
-            xln.Value = jump;
-        end
-
-        fig.Name = name;
-        ln.YData = x;
-        tl.String = name + " - " + string(jump);
-
-        drawnow();
-
-    end
-
-    dir = fullfile("data", "figures", string(jump));
-
-    if ~isfolder(dir)
-        mkdir(dir);
-    end
-
-    savefig(fig, fullfile(dir, name), "compact");
+    if ishandle(omniFig), close(omniFig); end
 
 end
